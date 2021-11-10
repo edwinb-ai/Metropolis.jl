aa6c07d74ec6860bdf54c564ed76390273d521c5
Edwin Bedolla <developeredwin@gmail.com>
2021-11-10 16:25:10 -0600

    :sparkles:
    Make it possible for the examples to run. This is done updating the
    necessary dependencies and scripts.

--- examples/npt-hs.jl
+++ examples/npt-hs.jl
@@ -1,10 +1,11 @@
 using Metropolis
 using Printf
 using JLD2
+using StaticArrays
 
 function run(syst, dispm, simul; pos=nothing)
     # Create the positions vector
-    positions = [zeros(SVector{3}) for n in 1:syst.N]
+    positions = [zeros(SVector{3}) for _ in 1:syst.N]
     if isnothing(pos)
         # Initialize the positions as grid
         init!(positions, syst)
@@ -18,8 +19,7 @@ function run(syst, dispm, simul; pos=nothing)
     # * Main loop
     # Equilibration steps
     move(positions, syst, simul.eq, dispm; volume=true)
-    JLD2.@save joinpath("data", positions_file) positions
-
+    JLD2.@save joinpath(@__DIR__, "data", positions_file) positions
 
     # Sampling steps
     move(
@@ -28,10 +28,10 @@ function run(syst, dispm, simul; pos=nothing)
         simul.sample,
         dispm;
         volume=true,
-        filename=joinpath("results", save_file),
+        filename=joinpath(@__DIR__, "results", save_file),
     )
 end
 
-(syst, dispm, simul, posfile) = parse_toml("npt.toml")
+(syst, dispm, simul, posfile) = parse_toml(joinpath(@__DIR__, "npt.toml"))
 
 run(syst, dispm, simul; pos=posfile)
