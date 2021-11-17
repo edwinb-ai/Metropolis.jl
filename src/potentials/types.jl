64f1cfd0274efad3589859645639616754d12b60
Edwin Bedolla <developeredwin@gmail.com>
2021-11-16 20:12:40 -0600

    :sparkles:
    Define a type hierarchy for the interaction potentials.

---
+++ src/potentials/types.jl
@@ -0,0 +1,22 @@
+"""
+    Potential
+
+Supertype for all kinds of interaction potentials.
+"""
+abstract type Potential end
+
+"""
+    Continuous <: Potential
+
+Type for all potentials that implement an potential energy
+and a force function. For example, the Lennard-Jones potential.
+"""
+abstract type Continuous <: Potential end
+
+"""
+    Discrete <: Potential
+
+Type for all potentials that only implement a potential energy, due
+to its discontinuous nature. For example, the hard-sphere potential.
+"""
+abstract type Discrete <: Potential end
