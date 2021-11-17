055bb06dceb618a8a431f28035fd72aafe4484bd
Edwin Bedolla <developeredwin@gmail.com>
2021-11-16 22:08:52 -0600

    :fire:
    There is no need for an initialization function because all particles
    will be generated randomly.

--- src/utils.jl
+++ src/utils.jl
@@ -7,36 +7,6 @@ function random_vec(::Type{VT}, range; rng=Random.GLOBAL_RNG) where {VT}
     return p
 end
 
-function init!(positions::AbstractArray, syst::System)
-    n3 = 2
-    ix = 0
-    iy = 0
-    iz = 0
-
-    # Find the lowest perfect cube, n3, greater than or equal to the
-    # number of particles
-    while n3^3 < syst.N
-        n3 += 1
-    end
-
-    for i in axes(positions, 1)
-        new_pos = SVector(
-            (ix + 0.5) * syst.L / n3, (iy + 0.5) * syst.L / n3, (iz + 0.5) * syst.L / n3
-        )
-        positions[i] = new_pos
-        ix += 1
-
-        if ix == n3
-            ix = 0
-            iy += 1
-            if iy == n3
-                iy = 0
-                iz += 1
-            end
-        end
-    end
-end
-
 function tofile(x::String, filename::String)
     open(filename, "a") do io
         println(io, x)
