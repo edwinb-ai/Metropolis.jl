38745e3f57583ef5bb58d6feb451ac08579e20b7
Edwin Bedolla <developeredwin@gmail.com>
2021-11-19 10:38:20 -0600

    :pencil2:
    Removed the random_init flag as it is no longer supported.

--- test/simulationtests.jl
+++ test/simulationtests.jl
@@ -3,7 +3,7 @@
     kT = 0.85
     particles = 500
     cutoff = 2.5
-    s = System(ρ, kT, particles, cutoff; random_init=true, lcell=1)
+    s = System(ρ, kT, particles, cutoff; lcell=1)
     sim = Simulation(s, NVT(0.25, 0.4), LennardJones())
     @test_nowarn simulate!(sim; steps=10_000)
 end
