3b4b97e7098c96a31ed6b3d56013d9b9229d89be
Edwin Bedolla <developeredwin@gmail.com>
2021-11-19 11:23:10 -0600

    :recycle:
    A complete refactoring to allow for different kind of pairwise
    computation of the particles.

--- src/simulate.jl
+++ src/simulate.jl
@@ -1,32 +1,55 @@
 function simulate! end
 
-function simulate!(sim::Simulation; steps=10_000, parallel=false, ishow=10_000)
-    @unpack system, ensemble, potential = sim
+function simulate!(sim::Simulation; pairwise=:cells, kwargs...)
+    if pairwise == :cells
+        _simulate_cells!(sim; kwargs...)
+    else
+        _simulate_squared!(sim; kwargs...)
+    end
 
+    return nothing
+end
+
+function _setup_simulation(sim::Simulation; kwargs...)
     # Obtain the energy function for the interaction potential
-    (uij, fij) = interactions(potential)
+    (uij, fij) = interactions(sim.potential)
 
     # Build initial cell lists
-    cl = CellListMap.CellList(system.xpos, system.box; parallel=parallel)
-
-    # Create the ensemble options
-    opts = EnsembleOptions(ensemble)
+    cl = CellListMap.CellList(sim.system.xpos, sim.system.box; parallel=kwargs[:parallel])
 
     # Create a good enough configuration
-    packsystem!(system, cl, uij)
+    packsystem!(sim.system, cl, uij)
 
-    # * Simulation loop
+    return uij, fij, cl
+end
+
+# Simulation loop
+function _simulation_loop!(system::System, uij, fij, opts, steps; kwargs...)
     for istep in 1:steps
         opts.nattempt += 1
-        uener = mcmove!(system, uij, fij, opts, cl; parallel=parallel)
+        uener = mcmove!(system, uij, fij, opts, kwargs[:cl]; parallel=kwargs[:parallel])
 
-        if istep % ishow == 0
+        if istep % kwargs[:ishow] == 0
             @show uener / system.npart
             @show opts.naccept / opts.nattempt
         end
 
         adjust!(opts)
     end
+
+    return nothing
+end
+
+function _simulate_cells!(sim::Simulation; steps=10_000, parallel=false, ishow=10_000)
+    @unpack system, ensemble, potential = sim
+
+    # Obtain the energy function for the interaction potential
+    (uij, fij, cl) = _setup_simulation(sim; parallel=parallel)
+
+    # Create the ensemble options
+    opts = EnsembleOptions(ensemble)
+
+    _simulation_loop!(system, uij, fij, opts, steps; cl=cl, parallel=parallel, ishow=ishow)
 
     return nothing
 end
